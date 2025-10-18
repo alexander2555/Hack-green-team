13cadf0349bc5d871da8a43dbee3ef1fc2a4d873
Dmitry Seregin <dimenerDS@gmail.com>
2025-10-18 11:13:10 +0400

    Button is added

--- src/components/control-panel/control-panel.jsx
+++ src/components/control-panel/control-panel.jsx
@@ -1,3 +1,16 @@
+import { Button } from "../../elements/button/button";
+import styles from "./control-panel.module.css";
+
 export const ControlPanel = () => {
-  return <div>ControlPanel</div>;
+  return (
+    <div className={styles.controlPanelContainer}>
+      <Button
+        lable="Главная"
+        textcolor="#fff"
+        backgroundColor="rgb(161, 38, 151)"
+        onClick={() => alert("Click")}
+        borderRadius="10px"
+      />
+    </div>
+  );
 };
